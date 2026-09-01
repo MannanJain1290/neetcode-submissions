fd9acddaed99abd0badc628947596846c75848f3
Mannan Jain <jainmannan8123@gmail.com>
2026-09-01 13:43:15 +0530

    Add: merge-strings-alternately - submission-2

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-2.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string res;
+        int n = word1.size();
+        int m = word2.size();
+        for(int i = 0;i < m || i < n;i++){
+            if(i < n) res += word1[i];
+            if(i < m) res += word2[i];
+        }
+        return res;
+    }
+};
